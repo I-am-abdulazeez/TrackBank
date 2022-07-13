dac41a00e3346ef5422911c7b89b4d974248a7d6
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-13 19:35:33 +0100

    chore(ui): AppContaimer component for routing and UI

---
+++ src/components/AppContainer/index.tsx
@@ -0,0 +1,17 @@
+import React, { type PropsWithChildren } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { ThemeProvider } from '@rneui/themed';
+
+import { NavigationTheme, RNETheme } from 'src/assets/theme';
+
+const Index = ({ children }: PropsWithChildren) => {
+  return (
+    <NavigationContainer theme={NavigationTheme}>
+      <ThemeProvider theme={RNETheme}>{children}</ThemeProvider>
+    </NavigationContainer>
+  );
+};
+
+export default Index;
