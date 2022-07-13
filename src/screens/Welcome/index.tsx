97298045cfbf75730b8eadb53d4afa01fda5f4df
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-13 19:36:44 +0100

    chore(ui): Welcome Screen

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Text } from '@rneui/themed';
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome Page</Text>
+    </View>
+  );
+};
+
+export default Welcome;
