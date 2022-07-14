3944a494f00bf9dc092ca5724973dff550c998c2
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 18:22:57 +0100

    chore: Added italic styles to font in constant folder

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,13 @@
 export const FONTS = {
   bold: 'AlbertSans-Bold',
+  boldItalic: 'AlbertSans-BoldItalic',
   semiBold: 'AlbertSans-SemiBold',
+  semiBoldItalic: 'AlbertSans-SemiBoldItalic',
   medium: 'AlbertSans-Medium',
+  mediumItalic: 'AlbertSans-MediumItalic',
   regular: 'AlbertSans-Regular',
   light: 'AlbertSans-Light',
+  lightItalic: 'AlbertSans-LightItalic',
 };
 
 export const AppLogo = require('../assets/images/logo.png');
