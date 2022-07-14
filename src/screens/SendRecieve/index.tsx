533cdd42b2cba0d6f7f792359ec9a9a1731fc8e5
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 12:04:52 +0100

    chore(ui): Added SendReceive Screen

---
+++ src/screens/SendRecieve/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SendRecieveScreen = () => {
+  return (
+    <View>
+      <Text>SendRecieveScreen</Text>
+    </View>
+  );
+};
+
+export default SendRecieveScreen;
