b981ad141ddba446c7e8577f4f2a64e842cf3907
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 12:02:48 +0100

    chore(ui): Added activeClass to icons + Second tab

--- src/components/BottomTabScreens/index.tsx
+++ src/components/BottomTabScreens/index.tsx
@@ -5,26 +5,50 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Icon } from '@rneui/themed';
 
 import HomeScreen from 'src/screens/Home';
+import SendRecieveScreen from 'src/screens/SendRecieve';
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabScreens = () => {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#00B894',
+        tabBarInactiveTintColor: '#B2E1D7',
+      }}>
       <BottomTab.Screen
         options={{
           headerShown: false,
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({ color, focused }) => (
             <Icon
-              name="home-outline"
+              name={focused ? 'home' : 'home-outline'}
               type="ionicon"
               color={color}
-              size={size}
+              size={22}
             />
           ),
         }}
         name="Home"
         component={HomeScreen}
+      />
+      <BottomTab.Screen
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Send/Receive',
+          tabBarIcon: ({ color, focused }) => (
+            <Icon
+              name={focused ? 'send' : 'send-outline'}
+              type="ionicon"
+              color={color}
+              size={23}
+              iconStyle={{
+                transform: [{ rotate: '-40deg' }],
+              }}
+            />
+          ),
+        }}
+        name="SendRecieve"
+        component={SendRecieveScreen}
       />
     </BottomTab.Navigator>
   );
