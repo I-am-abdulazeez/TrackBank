1571a6c876f31368544ee9c83fdaace46c88431f
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 11:18:07 +0100

    chore(ui): added button to navigate

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Text } from '@rneui/themed';
+import { StatusBar, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const Welcome = () => {
+import { Button, Text } from '@rneui/themed';
+import { RootStackParamList } from 'src/types';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type NavScreenProps = NativeStackNavigationProp<RootStackParamList>;
+
+const WelcomeScreen = () => {
+  const navigation = useNavigation<NavScreenProps>();
   return (
     <View>
       <Text>Welcome Page</Text>
+      <Button onPress={() => navigation.navigate('Dashboard')}>
+        Go to Home
+      </Button>
     </View>
   );
 };
 
-export default Welcome;
+export default WelcomeScreen;
