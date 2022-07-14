436dafc096cf6975b142b2927fd92df904cd3ad2
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 11:14:50 +0100

    chore(routes): Created navigation routes list

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+  Welcome: undefined;
+  Dashboard: undefined;
+  SendReceive: undefined;
+  Chart: undefined;
+  Settings: undefined;
+  Login: undefined;
+  Register: undefined;
+};
