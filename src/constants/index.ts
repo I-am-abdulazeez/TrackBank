66a21070ecc83716880fa4f1df607ddc4578b876
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-14 17:09:15 +0100

    chore(font): Added font family to constant

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const FONTS = {
+  bold: 'AlbertSans-Bold',
+  semiBold: 'AlbertSans-SemiBold',
+  medium: 'AlbertSans-Medium',
+  regular: 'AlbertSans-Regular',
+  light: 'AlbertSans-Light',
+};
