a454b70c0ce7b21dc6309fdfeb92e6162aa1c89a
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-22 20:04:34 +0100

    chore: created LoginScreen

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Text } from '@rneui/themed';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
